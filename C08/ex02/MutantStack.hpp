ead121fb76fc88308458440f866d2b8f0a7bb7fb
Tadeusz Kondracki <tadeusz.kondracki@gmail.com>
2022-05-08 18:50:05 +0200

    08 02 almost

---
+++ C08/ex02/MutantStack.hpp
@@ -0,0 +1,39 @@
+//
+// Created by tkondrac on 5/8/22.
+//
+
+#ifndef CPP_MODULES_MUTANTSTACK_HPP
+#define CPP_MODULES_MUTANTSTACK_HPP
+
+#include <stack>
+#include <vector>
+
+template<class T, class Container = std::vector<T> >
+class MutantStack: public std::stack<T, Container>
+{
+public:
+	typedef typename Container::iterator iterator;
+
+	MutantStack() { }
+	MutantStack(const MutantStack<T, Container>& inst) { *this = inst; }
+	explicit MutantStack( const Container& cont ): std::stack<T, Container>(cont) { }
+	~MutantStack() { };
+
+	MutantStack<T, Container> &operator=(const MutantStack<T, Container> & rhs){
+		this->c = rhs.c;
+		return *this;
+	}
+
+	iterator begin(){
+		return this->c.begin();
+	}
+	iterator end(){
+		return this->c.end();
+	}
+
+private:
+
+};
+
+
+#endif //CPP_MODULES_MUTANTSTACK_HPP
